ad59e96b1d7ea86a46ab9fbb3ea787f9e2c04b16
Joshua Cheng <22689840+PersonalPalimpsest@users.noreply.github.com>
2020-08-03 00:53:02 -0400

    automatically sort setups with current location to the top

--- src/favorite-setups.js
+++ src/favorite-setups.js
@@ -583,8 +583,26 @@
       // TODO: [med]  Mobile UX for drag & drop as well as scrollable div (jquery-ui-touch-punch did not work for simulating touch events)
       // TODO: [low]  Skin implementation/checks (in-progress, but either save for later or scrap entirely since use case is minimal)
 
-      // Generate and append each saved setup as a new <tr>
+      // Sort setups from the current location to the top of the list
+      function locSort (name) {
+          //console.log("saved loc: "+savedSetups[name].location, "\n loc bool: "+!!savedSetups[name].location, "\n current loc: "+user.environment_name, "\n current loc bool: "+!!user.environment_name, "\n current loc is saved loc: "+(user.environment_name===savedSetups[name].location), "\n test: "+(!!savedSetups[name].location && user.environment_name == savedSetups[name].location));
+          if (user.environment_name === savedSetups[name].location) {
+              //console.log("saved loc: "+savedSetups[name].location, "\n loc bool: "+!!savedSetups[name].location, "\n current loc: "+user.environment_name, "\n current loc bool: "+!!user.environment_name, "\n current loc is saved loc: "+(user.environment_name===savedSetups[name].location), "\n test: "+(!!savedSetups[name].location && user.environment_name == savedSetups[name].location));
+              savedSetups[name].sort = -1;
+              //console.log("location sorted setup: "+savedSetups[name])
+          };
+      };
+
       savedSetupSortKeys.forEach(name => {
+          locSort(name);
+      });
+
+      const sortedSetupKeys = Object.keys(savedSetups).sort((a, b) => {
+          return savedSetups[a].sort - savedSetups[b].sort;
+      });
+
+      // Generate and append each saved setup as a new <tr>
+      sortedSetupKeys.forEach(name => {
         generateRow(name);
       });
 
